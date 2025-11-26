28831fd9b471f44ed5a675af69762ccabcd9c160
risako <167521310+G-blooper@users.noreply.github.com>
2025-11-26 18:37:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ st.write("This is test.")
 path = os.path.join("logs", "IDlogin.txt")
 
 if st.button("Push"):
+    os.makedirs("test")
     if not os.path.exists(path):
         st.write("Path not found")
     else:
